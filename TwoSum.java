472a95d725ca7011fecedbfaeb89be78a9181afc
Tarun <testinggyan.tg@gmail.com>
2022-12-11 17:52:41 +0530

    Two Sum

--- TwoSum.java
+++ TwoSum.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class TwoSum {
@@ -17,7 +18,7 @@ public class TwoSum {
         int []res=new int[2];
         for(int i=0;i<n;i++){ //loop to pick one array element at a time
 
-            for(int j=0;j<n;j++){  //loop to check with elements next to picked array element
+            for(int j=i+1;j<n;j++){  //loop to check with elements next to picked array element
 
                 if((arr[i]+arr[j])==target){ //comparing sum to target
                     res[0]=i;
@@ -65,18 +66,58 @@ public class TwoSum {
     }
 
 
+    /*
+     * TC = O(nlogn)+O(n)
+     * 
+     * SC=O(1)
+     * 
+     * 
+     */
+
+    public static int [] twoSumTwoPointer(int [] arr,int target){
+
+        int [] res=new int[2];
+
+        int n=arr.length;
+
+        int i=0;
+        int j=n-1;
+
+        Arrays.sort(arr);
+
+        while(i<=j){
+
+            if(arr[i]+arr[j]==target){
+                res[0]=i;
+                res[1]=j;
+                return res;
+            }
+            else if(arr[i]+arr[j]<target){
+                i++;
+            }
+            else if(arr[i]+arr[j]>target){
+                j--;
+            }
+
+        }
+
+        return res;
+    }
 
 
     public static void main(String[] args) {
 
 
-        int [] arr={1,2,3,4};
+        int [] arr={1,2,5,4,3};
  
-        int [] res=twoSumB(arr, 9);
+        int [] res=twoSumB(arr, 8);
         System.out.println("("+res[0]+","+res[1]+")");
 
-        int [] res1=twoSumE(arr, 9);
+        int [] res1=twoSumE(arr, 8);
         System.out.println("("+res1[0]+","+res1[1]+")");
+
+       int [] res2=twoSumTwoPointer(arr, 8);
+       System.out.println("("+res2[0]+","+res2[1]+")");
 
         
     }
