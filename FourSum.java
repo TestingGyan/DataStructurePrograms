1cf21f0e19b039d304c6471996745498fa5cc328
Tarun <testinggyan.tg@gmail.com>
2022-12-14 16:04:23 +0530

    fours sum & pascal triangle

--- FourSum.java
+++ FourSum.java
@@ -1,28 +1,40 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FourSum {
 
+/*
+ * TC : O(n^3)
+ * 
+ * Sc : O(n^2)
+ * 
+ * 
+ */
+    public static List<List<Integer>> fourSumE(int []arr,int target){
 
-    public static List<List<Integer>> fourSumE(int [] arr, int target){
+        List<List<Integer>> res= new ArrayList<>();
 
-        List<List<Integer>> res=new ArrayList<>();
+        int n =arr.length;
 
-        int n=arr.length;
+        if(arr==null || arr.length==0){ //checking if input array is empty
+            return res;
+        }
+        Arrays.sort(arr);  //sorting array O(nlogn)
 
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){  //loop for first ele check
 
-            int target2=target-arr[i];
+            int target2=target-arr[i]; 
 
-            for(int j=i+1;j<n;j++){
+            for(int j=i+1;j<n;j++){ //loop for second ele check
 
-                int target3=target2-arr[j];
-
-                int front=j+1;
+                int target3=target2-arr[j]; //target which will be sum of remaining two elements in quad if i & j are first two eles
+                int front=j+1; 
                 int back=n-1;
 
-                while(front<back){
+                while(front<back){ // applying two sum approach to find remaining elemnts in quad with respect to first (i) and second ele (j)
                     int twoSum=arr[front]+arr[back];
+
                     if(twoSum<target3){
                         front++;
                     }
@@ -32,21 +44,23 @@ public class FourSum {
                     else{
                         List<Integer> quad=new ArrayList<>();
                         quad.add(arr[i]);
-                        quad.add(arr[j]);
-                        quad.add(arr[front]);
+                        quad.add((arr[j]));
+                        quad.add((arr[front]));
                         quad.add(arr[back]);
                         res.add(quad);
 
-                        while(front<back && quad.get(2)==arr[front])++front;
-
-                        while(front<back && quad.get(3)==arr[back])--back;
-
+                        while(front<back && quad.get(2)==arr[front])++front;  // ensuring to skip duplicates of third ele in quad
+                        while(front<back && quad.get(3)==arr[back])--back; //ensuring to skip duplicates of fourth ele in quad
                     }
                 }
-                while(j+1<n&&arr[j+1]==arr[j])++j;
+
+                while(j+1<n && arr[j]==arr[j+1])++j; //ensuring to skip duplicates of second ele in quad
+                //here j+1 ensure to rech last dupliacte as j will also be incremented in for loop
+                
             }
 
-            while(i+1<n&&arr[i+1]==arr[i])++i;
+            while(i+1<n && arr[i+1]==arr[i])++i;//ensuring to skip duplicates of first ele in quad
+            //here i+1 ensure to rech last dupliacte as i will also be incremented in for loop
         }
 
         return res;
--- PascalTriangle.java
+++ PascalTriangle.java
@@ -3,37 +3,42 @@ import java.util.List;
 
 public class PascalTriangle {
 
-    public static List<List<Integer>> printPascal(int numRows){
+    /*
+     * 
+     * TC : O(n^2), as we are traversing to create a 2 d array across rows and cols based on input
+     * 
+     * SC : O(n^2), as we are storing result in List of List
+     *  
+     * 
+     */
+    public static List<List<Integer>> printPascal(int n){
 
         List<List<Integer>> res=new ArrayList<>();
 
         List<Integer> row,prev=null;
 
-        for(int i=0;i<numRows;i++){
+        for(int i=0;i<n;i++){  //loop for rows
 
-            row=new ArrayList<>();
+            row=new ArrayList<>();  // intializing list to store col elements of one row
+  
+            for(int j=0;j<=i;j++){  //loop for columns, that is why j<=i as in 2nd row there will be 2 eles or columsns, in 3 row there will be 3 like so
 
-            for(int j=0;j<=i;j++){
-
-                if(j==0 || j==i){
+                if(j==0 || j==i){ //as first and last ele of row will always be 1 in pascal triangle so inserting 1 in row
                     row.add(1);
                 }
                 else{
-                    row.add(prev.get(j)+prev.get(j-1));
-                }
-
-                
+                    row.add(prev.get(j)+prev.get(j-1)); //else it will be sum of prev row ele in current col and one col behind 
+                }          
             }
-            prev=row;
-            res.add(row);
+            prev=row; //updating prev row for next traversal
+            res.add(row); //addingthe created row in resultant list 
         }
-
         return res;
     }
 
     public static void main(String[] args) {
 
-        List<List<Integer>> res=printPascal(4);
+        List<List<Integer>> res=printPascal(2);
 
         for (List<Integer> list : res) {
             System.out.println(list);
