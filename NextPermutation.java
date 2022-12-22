14a91e654e359de1dbd43e1b56f68d71e2aefb65
Tarun <testinggyan.tg@gmail.com>
2022-12-22 18:21:13 +0530

    Next Permutation

---
+++ NextPermutation.java
@@ -0,0 +1,46 @@
+public class NextPermutation {
+
+/*
+ * TC=O(n)
+ * SC=O(1)
+ * 
+ */
+    public static void nextPermutation(int [] arr){
+        int n=arr.length;
+
+        if(arr==null || arr.length<=1){
+            return;
+        }
+        int i=n-2;
+        while(i>=0&&arr[i]>=arr[i+1])i--;
+        if(i>=0){
+            int j=n-1;
+            while(arr[j]<=arr[i])j--;
+            swap(arr,i,j);
+            
+        }
+        reverse(arr, i+1, n-1);
+    }
+
+    public static void swap(int [] arr,int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+
+    public static void reverse(int []arr,int i,int j){
+        while(i<j){
+            swap(arr,i++,j--);
+        }
+    }
+    public static void main(String[] args) {
+
+        int [] arr={1,3,5,4,2};
+        nextPermutation(arr);
+        for (int i : arr) {
+            System.out.println(i);
+        }
+        
+    }
+    
+}
